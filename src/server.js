29f05ca46ff8e10a3d166300229fa9ee49a61adf
Vered Rekanati <veredrec@gmail.com>
2017-09-13 17:58:55 +0300

    change to test conflict

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
 app.listen(app.get('port'), () => {
-  console.log('App running on port: ', app.get('port'));
+  console.log('App running on port', app.get('port'));
 });
