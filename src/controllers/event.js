dfddeebebd2d7c47e0f9691b190956c85d678a93
Vered Rekanati <veredrec@gmail.com>
2017-09-14 15:23:21 +0300

    add content to event.js and events.js Relates #60

---
+++ src/controllers/event.js
@@ -0,0 +1,9 @@
+const events = require('./../database/db_connection.js');
+
+exports.get = (req, res, next) => {
+  const { event } = req.params;
+  if (events.includes(event)) {
+    return res.render('event', { event });
+  }
+  next();
+};
---
+++ src/controllers/events.js
@@ -0,0 +1,5 @@
+const events = require('./../database/db_connection.js');
+
+exports.get = (req, res) => {
+  res.render('events', events);
+};
