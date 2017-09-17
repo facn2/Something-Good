8ebba00b278cf77f9f26f7a86b4d3efbd51ffca9
Vered Rekanati <veredrec@gmail.com>
2017-09-17 12:18:11 +0300

    changes to files in src to render pages with server and app.js Relates #59

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const controllers = require('./controllers/index');
+const helpers = require('./views/helpers/index');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Something Good is happening here');
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs',
+exphbs({
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials'),
+  defaultLayout: 'main',
+  helpers: helpers
+})
+);
+// app.get('/', (req, res) => {
+//   res.render('home', home);
+// });
+//
+// // app.get('/', (req, res) => {
+// //   res.send('Something Good is happening here');
+// // });
+//
+// app.get('/events', (req, res) => {
+//   res.render('events', events);
+// });
 
-app.get('/events', (req, res) => {
-  res.send('We have some amazing events to share with you!');
-});
+app.set('port', process.env.PORT || 5000);
+
+app.use(controllers);
 
 module.exports = app;
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const home = require('./');
-const events = require('./events');
-const event = require('./event');
-const create = require('./create');
-const thanks = require('./thanks');
-const error = require('./error');
+// const events = require('./events');
+// const event = require('./event');
+// const create = require('./create');
+// const thanks = require('./thanks');
+// const error = require('./error');
 
-router.get('/', home);
-router.get('/events', events.get);
-router.get('/event', event.get);
-router.get('/create', create.get);
-router.get('/thanks', thanks.get);
+router.get('/', (req, res) => {
+  res.render('home', home);
+});
 
-router.use(error.client);
-router.use(error.server);
+// router.get('/', home);
+// router.get('/events', events);
+// router.get('/event', event);
+// router.post('/create', create);
+// router.get('/thanks', thanks);
+
+// router.use(error.client);
+// router.use(error.server);
 
 module.exports = router;
